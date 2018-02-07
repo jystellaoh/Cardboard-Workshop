b54e45ecf120c4aa6886204dc00992622e110e52
Daphne Liu <daphnel15113455@gmail.com>
2018-02-06 22:29:27 -0800

    final version

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    void MuseumGrounds()
+    {
+        SceneManager.LoadScene("MuseumGrounds");
+    }
+
+    void Galleries()
+    {
+        SceneManager.LoadScene("Galleries");
+    }
+}
